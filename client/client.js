b6030383a23bee25b342297d626d2f12bbe8ef7b
Rajat Bhargava <rbhargava@walmartlabs.com>
2015-05-04 21:42:30 -0700

    fixed issue of not deleting

--- client/client.js
+++ client/client.js
@@ -24,9 +24,13 @@ socket.on('connect', function() { //Don't send until we're connected
     socket.on('message', function(json) {
        async()=>{
         let action = json.action
-        let filepath=''
-        let filePath = path.join(ROOT_DIR,json.path)
-         // console.log("filepath:"+ filepath)
+        let filePath = ""
+        
+        filePath = path.join(ROOT_DIR,json.path)
+        console.log("filepath:"+ filePath)
+        console.log(e.stack)
+        }
+        
        let isDir = json.type === 'dir' ? true : false
        let dirPath =  isDir ? filePath : path.dirname(filePath)
        
@@ -40,20 +44,22 @@ socket.on('connect', function() { //Don't send until we're connected
         } else if(action === 'delete'){
             //Known Issue: Filepath does not get populated here. 
             if(isDir){
-                 await rimraf.promise(filepath).then(console.log("Directory Deleted")).catch(console.log)
+                
+                console.log(filePath)
+                 await rimraf.promise(filePath).then(console.log("Directory Deleted")).catch(console.log)
                  return
              } else {
-                await rimraf.promise.unlink(filepath).then(console.log('File Deleted')).catch(console.log)
+                await rimraf.promise.unlink(filePath).then(console.log('File Deleted')).catch(console.log)
             }
             return
         } else if(action === 'update'){
             //Known Issue: Filepath does not get populated here.
-            await fs.promise.truncate(filepath, 0)
+            await fs.promise.truncate(filePath, 0)
             await fs.promise.writeFile(filePath, json.contents).then(console.log('File updated at: '+ filePath))
         }
       
 
-       }().catch(console.log(e.message))
+       }().catch(console.log)
         
     });
     
